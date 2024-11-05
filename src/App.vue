27428635bab602b2f2d4b7053bc86de062123066
Vishnu <thenormvg@gmail.com>
2024-11-05 23:03:26 +0530

    bhai animated background added

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,38 @@ import NavBar from './components/NavBar.vue'
 
 <template>
   <NavBar />
+
+  <div id="vl-marquee">
+    <Vue3Marquee :clone="true" :duration="5">
+      <span> Gamma Vantage •</span>
+    </Vue3Marquee>
+  </div>
+  <div id="vr-marquee">
+    <Vue3Marquee :clone="true" :duration="5">
+      <span> Gamma Vantage • </span>
+    </Vue3Marquee>
+  </div>
   <RouterView />
 </template>
 
-<style scoped>
+<style scoped >
+span {
+  margin-right: 5px;
+}
+#vl-marquee {
+  font-family: 'Advent Pro', sans-serif;
+  position: fixed;
+  transform: rotate(90deg);
+  right: -99%;
+  z-index: -1;
+  color: #828a9c;
+}
+#vr-marquee {
+  font-family: 'Advent Pro', sans-serif;
+  position: fixed;
+  transform: rotate(-90deg);
+  z-index: -1;
+  left: -99%;
+  color: #828a9c;
+}
 </style>
